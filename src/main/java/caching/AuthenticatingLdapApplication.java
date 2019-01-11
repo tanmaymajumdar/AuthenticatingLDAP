f601a247afcfab32a0d85f8283dcffd678cea22b
tanmay_majumdar <tanmay.majumdar@nowfloats.com>
2019-01-12 03:53:41 +0530

    first commit

---
+++ src/main/java/caching/AuthenticatingLdapApplication.java
@@ -0,0 +1,14 @@
+package caching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatingLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatingLdapApplication.class, args);
+	}
+
+}
+
